a6d435659cdd66e01753ce72186e40ab2a96ea95
Leonardo Romor <leonardo.romor@gmail.com>
2020-10-12 20:53:29 +0200

    Added grid

--- ui/src/components/ThreeDimView.vue
+++ ui/src/components/ThreeDimView.vue
@@ -5,6 +5,7 @@
 
 <script>
 import * as Three from 'three'
+import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls.js'
 
 export default {
   name: 'ThreeDimView',
@@ -13,17 +14,29 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
-      mesh: null
+      mesh: null,
+      controls: null
     }
   },
   methods: {
     init: function () {
       const container = document.getElementById('container')
 
-      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 100)
       this.camera.position.z = 1
+      this.camera.position.x = 1
+      this.camera.position.y = 0.5
+      this.camera.lookAt(0.0, 1, 0.0)
 
       this.scene = new Three.Scene()
+
+      this.scene.background = new Three.Color(0xf0f0f0)
+
+      var helper = new Three.GridHelper(5, 100)
+      helper.position.y = 0
+      helper.material.opacity = 0.25
+      helper.material.transparent = true
+      this.scene.add(helper)
 
       const geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
       const material = new Three.MeshNormalMaterial()
@@ -33,7 +46,15 @@ export default {
 
       this.renderer = new Three.WebGLRenderer({ antialias: true })
       this.renderer.setSize(container.clientWidth, container.clientHeight, false)
+
       container.appendChild(this.renderer.domElement)
+      this.controls = new TrackballControls(this.camera, this.renderer.domElement)
+
+      this.controls.rotateSpeed = 1.0
+      this.controls.zoomSpeed = 1.2
+      this.controls.panSpeed = 0.8
+
+      this.controls.keys = [65, 83, 68]
     },
 
     resizeCanvasToDivSize: function () {
@@ -50,6 +71,7 @@ export default {
         this.renderer.setSize(width, height, false)
         this.camera.aspect = width / height
         this.camera.updateProjectionMatrix()
+        this.controls.handleResize()
       }
     },
 
@@ -57,8 +79,10 @@ export default {
       this.resizeCanvasToDivSize()
 
       requestAnimationFrame(this.animate)
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y += 0.02
+
+      this.controls.update()
+      // this.mesh.rotation.x += 0.01
+      // this.mesh.rotation.y += 0.02
       this.renderer.render(this.scene, this.camera)
     }
   },
@@ -73,7 +97,6 @@ export default {
   #container {
     width: 100%;
     height: 100%;
-    background: blue;
     line-height: 0;
   }
 
